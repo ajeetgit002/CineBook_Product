f822fc22bf10864db771697ebe14fa069c2a066d
ajeetgit002 <156986779+ajeetgit002@users.noreply.github.com>
2026-06-05 10:57:58 +0530

    Update RefreshTokenRepository.java

--- src/main/java/com/cinebook/repository/RefreshTokenRepository.java
+++ src/main/java/com/cinebook/repository/RefreshTokenRepository.java
@@ -1,14 +1,18 @@
-
-        package com.cinebook.repository;
+package com.cinebook.repository;
 
 import com.cinebook.entity.RefreshToken;
+import com.cinebook.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RefreshTokenRepository
         extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByToken(String token);
-}
 
+    List<RefreshToken> findAllByUser(User user);
+
+    void deleteByUser(User user);
+}
